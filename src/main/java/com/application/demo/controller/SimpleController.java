97b145dc2fe8e2dcb7a1ef3a6fddcc8017b1462d
touchman <beda67@mail.ru>
2021-10-04 02:03:08 +0300

    initial

---
+++ src/main/java/com/application/demo/controller/SimpleController.java
@@ -0,0 +1,15 @@
+package com.application.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/names")
+    public List<String> get() {
+        return List.of("Sergey", "Danila");
+    }
+}
